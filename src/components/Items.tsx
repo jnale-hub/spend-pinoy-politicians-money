f1ebaa1887e9ca6d1cd1b4cd35e1f41cb0c1c0b2
John Mark Delima <delimajohnmark14@gmail.com>
2025-01-04 19:00:25 +0800

    Adjust grid gap and padding for Items component for improved layout consistency

--- src/components/Items.tsx
+++ src/components/Items.tsx
@@ -6,7 +6,7 @@ const Items = () => {
   const { items } = useMoney();
 
   return (
-    <div className="relative grid sm:gap-4 gap-2 grid-cols-2 md:grid-cols-3 px-1 sm:px-4">
+    <div className="relative grid sm:gap-4 gap-3 grid-cols-2 md:grid-cols-3 px-1 sm:px-4">
       {items.map((item, index) => {
         const discountPercentage = item.oldPrice
           ? ((item.oldPrice - item.price) / item.oldPrice) * 100
@@ -15,19 +15,19 @@ const Items = () => {
         return (
           <div
             key={index}
-            className="relative w-full bg-white rounded-lg shadow-md hover:shadow-lg grid"
+            className="relative w-full bg-white rounded-lg shadow-md hover:shadow-lg"
           >
             <img
               src={item.img}
               alt={item.name}
               className="mx-auto aspect-square object-contain w-full"
             />
-            <div className="px-4 pt-4">
-              <h2 className="">{item.name}</h2>
-              <p className="text-orange-600 font-semibold text-2xl">
+            <div className="sm:px-4 px-3 pt-4">
+              <h2 className="text-sm md:text-lg leading-tight mb-1">{item.name}</h2>
+              <p className="text-orange-600 font-semibold tetxt-xl md:text-2xl break-words leading-tight">
                 {formatMoney(item.price)}
               </p>
-              <div className="flex justify-right items-center gap-2 text-sm mb-2">
+              <div className="flex justify-right items-center gap-2 md:text-sm text-xs mb-2">
                 {item.oldPrice && (
                   <span className="text-gray-400 line-through">
                     {formatMoney(item.oldPrice)}
@@ -38,8 +38,8 @@ const Items = () => {
                 )}
               </div>
             </div>
-            <div className="px-4 pb-4">
-              <div className="grid grid-cols-2 gap-4">
+            <div className="sm:px-4 px-3 pb-4">
+              <div className="grid grid-cols-2 sm:gap-4 gap-2">
               <Button index={index} className="sellBtn">
   Sell
 </Button>
