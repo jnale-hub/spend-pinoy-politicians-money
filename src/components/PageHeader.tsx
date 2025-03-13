449539a487aa2184e5b5be23edc840c048ce8513
John Mark Delima <delimajohnmark14@gmail.com>
2025-03-13 20:43:33 +0800

    Add Cynthia Villar image to PageHeader component

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,7 +1,7 @@
 const PageHeader = () => {
   return (
     <div className="bg-white p-8 rounded-lg shadow-md">
-      <img src="image.png" alt="Cynthia Villar" className="mx-auto p-4 size-40 rounded-full object-cover" />
+      <img src="/politicians/cynthia-villar.png" alt="Cynthia Villar" className="mx-auto p-4 size-40 rounded-full object-cover" />
 <h1 className="font-semibold text-3xl text-center">Spend Cynthia Villar's Money</h1>
     </div>
   )
